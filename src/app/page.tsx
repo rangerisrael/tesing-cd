58925f6ce9472fc648f617d56aac88d3e919b986
rangerisrael <israelalisoso1@gmail.com>
2025-06-14 22:01:44 +0800

    content delivery experiment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam
-      repudiandae deleniti, inventore vel neque officia cupiditate quibusdam
-      saepe quisquam rerum nesciunt. Nesciunt, eos. Nisi qui, delectus hic animi
-      incidunt voluptate?
     </div>
   );
 }
